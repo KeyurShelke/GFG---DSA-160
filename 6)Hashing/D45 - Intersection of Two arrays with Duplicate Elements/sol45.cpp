749e344a9c03cc3f4831ecd731f5facae49fe7a4
Keyur Shelke <shelkekeyur@gmail.com>
2025-05-12 13:16:07 +0530

    D45 added

---
+++ 6)Hashing/D45 - Intersection of Two arrays with Duplicate Elements/sol45.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> intersectionWithDuplicates(vector<int>& a, vector<int>& b) {
+        // code here
+        unordered_set<int> arr(a.begin(),a.end());
+        vector<int> sol;
+        for(int i=0; i<b.size();i++)
+        {
+            if(arr.find(b[i])!=arr.end())
+            {
+                sol.push_back(b[i]);
+                arr.erase(b[i]);
+            }
+        }
+        return sol;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr1, arr2;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr1.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            arr2.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> res = ob.intersectionWithDuplicates(arr1, arr2);
+        sort(res.begin(), res.end());
+
+        if (res.size() == 0) {
+            cout << "[]" << endl;
+        } else {
+            for (auto it : res)
+                cout << it << " ";
+            cout << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
