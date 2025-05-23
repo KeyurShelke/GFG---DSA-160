afe1005ddd51585c2ee1ad96caf0be853eb404e8
Keyur Shelke <shelkekeyur@gmail.com>
2025-05-23 11:35:14 +0530

    (7)Two Pointers/D55 - Count the number of possible triangles

---
+++ 7)Two Pointer Technique/D55 - Count the number of possible triangles/sol55.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    // Function to count the number of possible triangles.
+    int countTriangles(vector<int>& arr) {
+        // code here
+        int res=0;
+        sort(arr.begin(),arr.end());
+        for(int i = 2;i<arr.size();++i){
+            int left = 0,right = i-1;
+            while(left<right){
+                if(arr[left]+ arr[right] > arr[i]){
+                    res+= right - left;
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return res;
+    }
+};
