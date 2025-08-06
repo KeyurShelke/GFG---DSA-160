d16d256c1048aac3b122057dfb7dbfac52958d6d
Keyur Shelke <shelkekeyur@gmail.com>
2025-08-06 21:14:05 +0530

    DP / D126 - Stock Buy and Sell – Max 2 Transactions Allowed

---
+++ 15)DP/D126 - Stock Buy and Sell – Max 2 Transactions Allowed/sol.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        // code here
+        int firstBuy = INT_MIN;
+        int firstSell = 0;
+        int secondBuy =  INT_MIN;
+        int secondSell = 0;
+        
+        for(int i = 0; i < prices.size(); i++){
+            firstBuy = max(firstBuy, -prices[i]);
+            firstSell = max(firstSell, firstBuy + prices[i]);
+            
+            secondBuy = max(secondBuy, firstSell - prices[i]);
+            secondSell = max(secondSell, secondBuy + prices[i]);
+        }
+        return secondSell;
+    }
+};
