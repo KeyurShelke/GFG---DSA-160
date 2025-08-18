60c1de518daba540536a2211593371f6a0f576f9
Keyur Shelke <shelkekeyur@gmail.com>
2025-08-18 20:01:27 +0530

    Tries \ D156 - Maximum XOR of Two Numbers in an Array  comments

---
+++ 17)Tries/D156 - Maximum XOR of Two Numbers in an Array  comments/sol.cpp
@@ -0,0 +1,66 @@
+// User function Template for C++
+struct Node{
+    Node* links[2];
+    bool containsKey(int bit){
+        return links[bit]!= NULL;
+    }
+    void put(int bit,Node* node){
+        links[bit] = node;
+    }
+    Node* get(int bit){
+        return links[bit];
+    }
+};
+
+class Trie{
+    private:
+    Node* root;
+    public:
+    Trie(){
+        root=new Node();
+    }
+    void insert(int &num){
+        Node* node=root;
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1;
+            if(!node->containsKey(bit)){
+                node->put(bit,new Node());
+            }
+            node=node->get(bit);
+        }
+    }
+    int getMaxXor(int &num){
+        int ans=0;
+        Node *node=root;
+        for(int i=31;i>=0;i--){
+            int bit=(num>>i)&1;
+            if(node->containsKey(1-bit)){
+                ans=ans|(1<<i);
+                node=node->get(1-bit);
+            }else{
+                node=node->get(bit);
+            }
+        }
+        return ans;
+    }
+    
+};
+class Solution {
+  public:
+    int maxXor(vector<int> &arr) {
+        // code here
+        Trie trie;
+        for(int &num:arr){
+            trie.insert(num);
+        }
+        int maxXOR=0;
+        for(int &num:arr){
+            trie.insert(num);
+        }
+        int naxXOR=0;
+        for(int &num:arr){
+            maxXOR=max(maxXOR,trie.getMaxXor(num));
+        }
+        return maxXOR;
+    }
+};
